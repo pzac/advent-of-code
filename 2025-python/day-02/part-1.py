5fe035b1ceed9184f3ca35110a12913d06342ca6
Paolo Zaccagnini <paolo@pzac.net>
2025-12-02 09:14:53 +0100

    Day 2 part 1

---
+++ 2025-python/day-02/part-1.py
@@ -0,0 +1,40 @@
+import sys
+
+
+def get_ranges():
+    row = sys.stdin.readlines()[0].strip()
+    ranges = row.split(",")
+    return ranges
+
+
+def expand_range(string):
+    tokens = list(map(int, string.split("-")))
+    numbers = list(range(tokens[0], tokens[1] + 1))
+    return numbers
+
+
+def all_numbers():
+    all = []
+    for range in get_ranges():
+        all = all + expand_range(range)
+    return all
+
+
+def valid_number(number):
+    st = str(number)
+    size = len(st)
+    if size % 2 == 1:
+        return True
+    half_size = size // 2
+    valid = st[:half_size] != st[half_size:]
+    return valid
+
+
+all = all_numbers()
+
+sum = 0
+for num in all:
+    if not valid_number(num):
+        sum += num
+
+print("Sum:", sum)
